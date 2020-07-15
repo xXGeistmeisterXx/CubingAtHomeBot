c5c519f8597bdaf46f3e1ad1381ea993212463bf
xXGeistmeisterXx <43475307+xXGeistmeisterXx@users.noreply.github.com>
2020-07-15 15:03:00 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -8,7 +8,7 @@ function full() {
     return(false);
   }
   else{
-    if(re.test(check("time6")){
+    if(check("time6")){
       document.getElementById("submit").disabled = false;
       document.getElementById("alltimes").style.visibility = "hidden"
       console.log("true");
