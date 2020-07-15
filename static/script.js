fbe3e5d65cab79d32c49bf5eecc1e870fb1ef8ef
xXGeistmeisterXx <43475307+xXGeistmeisterXx@users.noreply.github.com>
2020-07-15 14:56:34 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -7,45 +7,13 @@ function full() {
     console.log("false");
     return(false);
   }
-  else if(checkf()){
+  else{
     document.getElementById("submit").disabled = false;
     document.getElementById("alltimes").style.visibility = "hidden"
     console.log("true");
     return(true);
   }
 
-}
-
-function checkf() {
-  var re = /^([0-9]{1,2}:)?[0-9]{1,2}.[0-9]{2}$/
-  if(re.test(document.getElementById("time1").value) && re.test(document.getElementById("time2").value) && re.test(document.getElementById("time3").value) && re.test(document.getElementById("time4").value) && re.test(document.getElementById("time5").value)){
-    console.log("enabled");
-    document.getElementById("submit").disabled = false;
-    document.getElementById("alltimes").style.visibility = "hidden"
-    return(true);
-  }
-  else{
-
-      console.log("disabled");
-      var ele = "Some of your times are formatted incorrectly ( "
-      var first = true
-      var i;
-      for (i = 1; i <= 5; i++) {
-        if(!re.test(document.getElementById("time".concat(i.toString())).value)){
-          if(first){
-            ele = ele.concat(i.toString());
-            first = false;
-          }
-          else{
-            ele = ele.concat(", " + i.toString());
-          }
-      }
-      document.getElementById("alltimes").innerHTML = ele.concat(" )");
-      document.getElementById("alltimes").style.visibility = "visible"
-      document.getElementById("submit").disabled = true;
-      return(false);
-    }
-  }
 }
 
 function check(eid) {
@@ -60,6 +28,33 @@ function check(eid) {
     console.log("visible")
   }
 
-  checkf();
-  
+  if(re.test(document.getElementById("time1").value) && re.test(document.getElementById("time2").value) && re.test(document.getElementById("time3").value) && re.test(document.getElementById("time4").value) && re.test(document.getElementById("time5").value)){
+    console.log("enabled");
+    document.getElementById("submit").disabled = false;
+    document.getElementById("alltimes").style.visibility = "hidden"
+  }
+  else{
+
+    if(full()){
+
+      console.log("disabled");
+      var ele = "Some of your times are formatted incorrectly ( "
+      var first = true
+      var i;
+      for (i = 1; i <= 5; i++) {
+        if(!re.test(document.getElementById("time".concat(i.toString())).value)){
+          if(first){
+            ele = ele.concat(i.toString());
+            first = false;
+          }
+          else{
+            ele = ele.concat(", " + i.toString());
+          }
+        }
+      }
+      document.getElementById("alltimes").innerHTML = ele.concat(" )");
+      document.getElementById("alltimes").style.visibility = "visible"
+      document.getElementById("submit").disabled = true;
+    }
+  }
 }
