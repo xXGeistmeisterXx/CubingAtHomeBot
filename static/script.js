29ff956bf547418e3f2b8c6fe963b716a41cb2c2
xXGeistmeisterXx <43475307+xXGeistmeisterXx@users.noreply.github.com>
2020-07-15 14:50:42 -0400

    Update script.js

--- static/script.js
+++ static/script.js
@@ -7,7 +7,7 @@ function full() {
     console.log("false");
     return(false);
   }
-  else{
+  else if(checkf()){
     document.getElementById("submit").disabled = false;
     document.getElementById("alltimes").style.visibility = "hidden"
     console.log("true");
@@ -16,26 +16,15 @@ function full() {
 
 }
 
-function check(eid) {
-  var time = document.getElementById(eid).value
+function checkf() {
   var re = /^([0-9]{1,2}:)?[0-9]{1,2}.[0-9]{2}$/
-  if(re.test(time)) {
-    document.getElementById(eid + "i").style.visibility = "hidden";
-    console.log("hidden")
-  }
-  else{
-    document.getElementById(eid + "i").style.visibility = "visible";
-    console.log("visible")
-  }
-
   if(re.test(document.getElementById("time1").value) && re.test(document.getElementById("time2").value) && re.test(document.getElementById("time3").value) && re.test(document.getElementById("time4").value) && re.test(document.getElementById("time5").value)){
     console.log("enabled");
     document.getElementById("submit").disabled = false;
     document.getElementById("alltimes").style.visibility = "hidden"
+    return(true);
   }
   else{
-
-    if(full()){
 
       console.log("disabled");
       var ele = "Some of your times are formatted incorrectly ( "
@@ -50,11 +39,27 @@ function check(eid) {
           else{
             ele = ele.concat(", " + i.toString());
           }
-        }
       }
       document.getElementById("alltimes").innerHTML = ele.concat(" )");
       document.getElementById("alltimes").style.visibility = "visible"
       document.getElementById("submit").disabled = true;
+      return(false);
     }
   }
+}
+
+function check(eid) {
+  var time = document.getElementById(eid).value
+  var re = /^([0-9]{1,2}:)?[0-9]{1,2}.[0-9]{2}$/
+  if(re.test(time)) {
+    document.getElementById(eid + "i").style.visibility = "hidden";
+    console.log("hidden")
+  }
+  else{
+    document.getElementById(eid + "i").style.visibility = "visible";
+    console.log("visible")
+  }
+
+  checkf();
+  
 }
